354bd7b9ba36ac27f245c723656a54397a2978ff
riyaz <riyazpasha880@gmail.com>
2020-08-13 16:05:48 +0530

    added custome emthod to model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -7,8 +9,12 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+
     def __str__(self):
         return self.question_text
+
 
 class Choise(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
