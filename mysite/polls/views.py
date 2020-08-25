cff60c10abab254834c20415cb633b8be012461c
riyaz <riyazpasha880@gmail.com>
2020-08-25 11:27:06 +0530

    created extra views in polls app and mapped to respective urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,5 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at result of question %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
